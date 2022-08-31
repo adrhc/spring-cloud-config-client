d7e6331863e682d64d230a07434ad58e2162ac2b
Adrian Petre <adrian.petre@endava.com>
2022-08-31 18:55:44 +0300

    DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@Value("${foo}")
+	private String foo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World!";
+		return String.format("foo is %s! see http://127.0.0.1:8888/bar/default", foo);
 	}
 }
